01f7f28125ba85fd3f75d1af670969b243e48c4b
Bilal Aksoy <sloeyeleet@gmail.com>
2024-06-28 17:50:03 +0300

    Created profile card

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,3 +2,7 @@ export const validateEmail = (email) => {
     const re = /\S+@\S+\.\S+/
     return re.test(email)
 }
+
+export const getInitials = (name) => {
+    return name.split(' ').map((n) => n[0]).join('')
+}
