c7d2e14e8e404d5c8e3351e801da0c837e5fb705
Bilal Aksoy <sloeyeleet@gmail.com>
2024-07-05 16:17:35 +0300

    Removed search bar from nav bar when user is not authenticated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,12 +28,14 @@ const Navbar = ({ user, onSearchNote, handleClearSearch }) => {
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-      <SearchBar
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        handleSearch={handleSearch}
-        onClearSearch={onClearSearch}
-      />
+      {user && (
+        <SearchBar
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
+      )}
 
       {user && <ProfileInfo user={user} onLogout={onLogout} />}
     </div>
